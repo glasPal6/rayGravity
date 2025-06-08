457b5d90e76b9a566d4ec885c74c6c65b90b94ce
glaspal6 <dylan.r.kamstra@gmail.com>
2025-06-08 14:57:55 +0200

    feat(gravity): Update

--- src/main.c
+++ src/main.c
@@ -5,22 +5,22 @@
 #include "raymath.h"
 
 #define N_Obj 2
-#define GRAVITY_CONST 250.0
+#define GRAVITY_CONST 200.0
+// #define dt GetFrameTime()
+#define dt 0.1
 
 typedef struct {
     Vector2 pos;
     Vector2 vel;
     int16_t mass;
-    int16_t radius;
-    Color color;
 } Object;
 
-void update_objects(Object* objects, float dt) {
+void update_objects(Object* objects) {
     // Calculate all accelerations first
     Vector2 accelerations[N_Obj] = {0};
 
     for (int i = 0; i < N_Obj; i++) {
-        for (int j = 0; j < N_Obj; j++) {
+        for (int j = i + 1; j < N_Obj; j++) {
             if (j == i) continue;  // Skip self-interaction
 
             Vector2 r = Vector2Subtract(objects[j].pos, objects[i].pos);
@@ -36,58 +36,53 @@ void update_objects(Object* objects, float dt) {
                 r_n, GRAVITY_CONST * objects[j].mass / (distance * distance));
 
             accelerations[i] = Vector2Add(accelerations[i], acc);
+            accelerations[j] = Vector2Subtract(accelerations[j], acc);
         }
     }
 
     // Apply accelerations and update positions
     for (int i = 0; i < N_Obj; i++) {
-        // Update velocity based on acceleration
         objects[i].vel =
             Vector2Add(objects[i].vel, Vector2Scale(accelerations[i], dt));
-
-        // Update position based on velocity
         objects[i].pos =
             Vector2Add(objects[i].pos, Vector2Scale(objects[i].vel, dt));
     }
 }
 
 int main() {
-    InitWindow(800, 450, "Gravity Simulator");
+    InitWindow(1600, 900, "Gravity Simulator");
 
     Object objects[] = {
         {
             .pos = (Vector2){200.0f, 200.0f},
-            .vel = (Vector2){0.0f, 0.0f},
+            .vel = (Vector2){-5.0f, 0.0f},
             .mass = 1000,
-            .radius = 10.0f,
-            .color = BLUE,
         },
         {
             .pos = (Vector2){700.0f, 400.0f},
-            .vel = (Vector2){0.0f, -10.0f},
+            .vel = (Vector2){0.0f, 10.0f},
             .mass = 500,
-            .radius = 5.0f,
-            .color = RED,
         },
     };
 
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
         // Update
-        float dt = GetFrameTime();
-        update_objects(objects, dt);
+        update_objects(objects);
 
         // Draw
         BeginDrawing();
+        DrawFPS(10, 10);
         ClearBackground(RAYWHITE);
 
         for (int i = 0; i < N_Obj; i++) {
-            DrawCircleV(objects[i].pos, objects[i].radius, objects[i].color);
+            DrawCircleV(objects[i].pos, 1, BLACK);
         }
 
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
