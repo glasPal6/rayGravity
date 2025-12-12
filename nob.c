7b4c8485515febef7714209c89471d933cc757cb
glaspal6 <dylan.r.kamstra@gmail.com>
2025-12-12 11:04:17 +0200

    feat(compile): Fixed the link to the library

--- nob.c
+++ nob.c
@@ -9,19 +9,19 @@
 #define INCLUDE_DIR "./include/"
 #define LIBRARY_DIR "./lib/"
 
-void compile_flags(Nob_Cmd *cmd) { cmd_append(cmd, "-Wall", "-Wextra"); }
-void profile_debug_flags(Nob_Cmd *cmd) {
+void compile_flags(Nob_Cmd* cmd) { cmd_append(cmd, "-Wall", "-Wextra"); }
+void profile_debug_flags(Nob_Cmd* cmd) {
 #ifdef DEBUG
     cmd_append(cmd, "-ggdb");
 #endif /* ifdef DEBUG */
 }
 
-void include_dirs(Nob_Cmd *cmd) {
+void include_dirs(Nob_Cmd* cmd) {
     cmd_append(cmd, "-I", INCLUDE_DIR "raylib");
     cmd_append(cmd, "-I", INCLUDE_DIR "clay");
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
     if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
@@ -35,7 +35,8 @@ int main(int argc, char **argv) {
 
     include_dirs(&cmd);
     cmd_append(&cmd, "-o", BUILD_DIR "main", SRC_DIR "main.c", "-O3");
-    cmd_append(&cmd, "-L" LIBRARY_DIR "raylib", "-lraylib", "-lm");
+    cmd_append(&cmd, "-L" LIBRARY_DIR, "-lraylib", "-lm", "-lpthread",
+               "-Wl,-rpath=../lib");
 
     bool cmd_result = cmd_run_sync(cmd);
     cmd_free(cmd);
